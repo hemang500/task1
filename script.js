eb33f7b24ae06c0e2431a0fd25fbac5630c64323
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-03-19 16:04:18 +0530

    finish off funtionality

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-console.log("hello world ");
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+
+const personStatus=document.querySelector(".status-para");
+const approveButton=document.querySelector(".employee-details-approve-btn")
+const rejectButton=document.querySelector(".employee-details-reject-btn")
+
+
+console.log(approveButton);
+console.log(rejectButton);
+ 
+// console.log(personStatus.innerText ="Status: Rejected");
+
+
+
+
+function employeeApproveButton(){
+
+    
+    approveButton.addEventListener('click',()=>{
+        
+        let check=confirm("Are sure to Approved")
+    if(check){
+
+        personStatus.classList.add("status-para-approve")
+        personStatus.innerText = "Status: Approved";
+    }
+
+    approveButton.disabled = true;
+
+        
+   })
+
+   
+    
+    
+} 
+
+employeeApproveButton();
+
+function  employeeRejectButton(){ 
+
+            
+    rejectButton.addEventListener('click',()=>{
+        
+        let check=confirm("Are sure to Approved")
+    if(check){
+
+        personStatus.classList.add("status-para-approve")
+        personStatus.innerText = "Status: Approved";
+    }
+
+    approveButton.disabled = true;
+
+        
+   })
+
+    
+}
+
+
+
+
+
+
+// DOMContentLoaded
+});
